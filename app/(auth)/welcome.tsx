efeb5e706c36aea4ea66aff04e6baeb8de263d5a
Ibrahim Yakubu <ibrahimy437@gmail.com>
2024-08-25 23:10:06 +0100

    Design onboarding screen skip button

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,11 +1,20 @@
 import { Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { router } from "expo-router";
 
 const Onboarding = () => {
   return (
-    <SafeAreaView>
-      <Text>Welcome</Text>
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up" as never);
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text>Skip</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
