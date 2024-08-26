38fea085edf53a6e1ba5bd70ae7fd47f322ac1bf
Ibrahim Yakubu <ibrahimy437@gmail.com>
2024-08-26 23:57:17 +0100

    Install react-native-swiper for swiping through onboarding screens and create constants for onboarding screen

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,15 @@
 import { Text } from "react-native";
-import React from "react";
+import React, { useState, useRef } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { router } from "expo-router";
+import Swiper from "react-native-swiper";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+import { onboarding } from "@/constants";
 
 const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
@@ -13,8 +18,43 @@ const Onboarding = () => {
         }}
         className="w-full flex justify-end items-end p-5"
       >
-        <Text>Skip</Text>
+        <Text className="text-black text-md font-JakartaExtraBold">Skip</Text>
       </TouchableOpacity>
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full"></View>
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] mx-1 rounded-full"></View>
+        }
+        onIndexChanged={(index) => {
+          setActiveIndex(index);
+        }}
+      >
+        {onboarding.map((item) => {
+          return (
+            <View key={item.id} className="flex items-center justify-center">
+              <View className="w-full h-[400px]">
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  className="w-full h-full object-contain"
+                />
+              </View>
+              <View className="w-full px-5">
+                <Text className="text-black text-2xl font-JakartaExtraBold text-center">
+                  {item.title}
+                </Text>
+                <Text className="text-black text-base font-JakartaRegular text-center">
+                  {item.description}
+                </Text>
+              </View>
+            </View>
+          );
+        })}
+      </Swiper>
     </SafeAreaView>
   );
 };
---
+++ constants/index.ts
@@ -0,0 +1,23 @@
+export const onboarding = [
+  {
+    id: 1,
+    title: "Welcome to Uber",
+    image: require("../assets/images/onboarding-1.png"),
+    description:
+      "Order your favorite food from the best restaurants in your city.",
+  },
+  {
+    id: 2,
+    title: "Choose your food",
+    image: require("../assets/images/onboarding-2.png"),
+    description:
+      "Order your favorite food from the best restaurants in your city.",
+  },
+  {
+    id: 3,
+    title: "Enjoy your food",
+    image: require("../assets/images/onboarding-3.png"),
+    description:
+      "Order your favorite food from the best restaurants in your city.",
+  },
+];
